5fd1c84315f3753a8a5bf5f77182e3cde8a3eff5
Esraa Alshaikh <esraaalshaikh200@gmail.com>
2026-03-07 17:47:41 +0300

    Add VehicleApiClient for external API

---
+++ VehicleDataAPI/Clients/IVehicleApiClient.cs
@@ -0,0 +1,15 @@
+﻿using VehicleDataAPI.Models.Dtos;
+using VehicleDataAPI.Models.ResponseDtos;
+
+namespace VehicleDataAPI.Clients
+{
+    public interface IVehicleApiClient
+    {
+        Task<MakesResponseDTO> GetMakes();
+
+        Task<List<VehicleTypeDto>> GetVehicleTypes(int makeId);
+
+        Task<List<ModelDto>> GetModelsForMake(int makeId, int yearId);
+
+    }
+}
---
+++ VehicleDataAPI/Clients/VehicleApiClient.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using VehicleDataAPI.Models;
+using VehicleDataAPI.Models.Dtos;
+using VehicleDataAPI.Models.ResponseDtos;
+
+namespace VehicleDataAPI.Clients
+{
+    public class VehicleApiClient : IVehicleApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public VehicleApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<MakesResponseDTO> GetMakes()
+        {
+            var responseString = await _httpClient.GetStringAsync(
+                "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json");
+
+            using var doc = JsonDocument.Parse(responseString);
+            var root = doc.RootElement;
+
+            var makesList = root.GetProperty("Results")
+                .EnumerateArray()
+                .Select(r => new MakeDTO
+                {
+                    MakeId = r.GetProperty("Make_ID").GetInt32(),
+                    MakeName = r.GetProperty("Make_Name").GetString()
+                })
+                .ToList();
+
+            return new MakesResponseDTO
+            {
+                Count = root.GetProperty("Count").GetInt32(),
+                Message = root.GetProperty("Message").GetString(),
+                Makes = makesList
+            };
+        }
+
+        public Task<List<ModelDto>> GetModelsForMake(int makeId, int yearId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<VehicleTypeDto>> GetVehicleTypes(int makeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
