e3c25b5b841651ade08e552acaea7717241eaa79
Esraa Alshaikh <esraaalshaikh200@gmail.com>
2026-03-07 17:47:07 +0300

    Configure ASP.NET Core project and app settings

--- VehicleDataAPI/Program.cs
+++ VehicleDataAPI/Program.cs
@@ -1,3 +1,4 @@
+﻿using VehicleDataAPI.Clients;
 
 namespace VehicleDataAPI
 {
@@ -8,23 +9,26 @@ namespace VehicleDataAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            builder.Services.AddHttpClient<IVehicleApiClient, VehicleApiClient>();
+
+            // Add Swagger/OpenAPI services
+            builder.Services.AddEndpointsApiExplorer(); 
+            builder.Services.AddSwaggerGen();          
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();       
+                app.UseSwaggerUI();     
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
