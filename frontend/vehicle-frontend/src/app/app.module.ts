7bf64f8f70ca08716223aa4e4c4516f6e26d34a9
Esraa Alshaikh <esraaalshaikh200@gmail.com>
2026-03-10 00:55:25 +0300

    (frontend): Add Components

--- frontend/vehicle-frontend/src/app/app.module.ts
+++ frontend/vehicle-frontend/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { MakesComponent } from './Components/makes/makes.component';
+import { ModelsComponent } from './Components/models/models.component';
+import { VehicleComponent } from './Components/vehicle/vehicle.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MakesComponent,
+    ModelsComponent,
+    VehicleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
