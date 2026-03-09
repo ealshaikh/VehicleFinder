0187b9c2259b51e900fb383a64981b452d8e3e1f
Esraa Alshaikh <esraaalshaikh200@gmail.com>
2026-03-10 01:06:21 +0300

    refactor: Use camelCase for VehicleServices

--- frontend/vehicle-frontend/src/app/services/vehicle.service.ts
+++ frontend/vehicle-frontend/src/app/services/vehicle.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
-import { Model } from '../interfaces/model';
-import { VehicleType } from '../interfaces/vehicle';
 import { Make } from '../interfaces/make';
+import { VehicleType } from '../interfaces/vehicle';
+import { Model } from '../interfaces/model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +12,17 @@ import { Make } from '../interfaces/make';
 export class VehicleService {
   private baseUrl = environment.apiBaseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  GetMakes(page = 1, pageSize = 100): Observable<Make[]> {
+  getMakes(page = 1, pageSize = 100): Observable<Make[]> {
     return this.http.get<Make[]>(`${this.baseUrl}/makes?page=${page}&pageSize=${pageSize}`);
   }
 
-  GetVehicle(make: number, page = 1, pageSize = 100): Observable<VehicleType[]> {
-    return this.http.get<VehicleType[]>(`${this.baseUrl}?make=${make}&page=${page}&pageSize=${pageSize}`);
+  getVehicleTypes(makeId: number, page = 1, pageSize = 100): Observable<VehicleType[]> {
+    return this.http.get<VehicleType[]>(`${this.baseUrl}/types?makeId=${makeId}&page=${page}&pageSize=${pageSize}`);
   }
 
-  GetModels(make: number, modelYear: number, page = 1, pageSize = 100): Observable<Model[]> {
-    return this.http.get<Model[]>(`${this.baseUrl}/models?make=${make}&modelYear=${modelYear}&page=${page}&pageSize=${pageSize}`);
+  getModels(makeId: number, modelYear: number, page = 1, pageSize = 100): Observable<Model[]> {
+    return this.http.get<Model[]>(`${this.baseUrl}/models?makeId=${makeId}&modelYear=${modelYear}&page=${page}&pageSize=${pageSize}`);
   }
 }
