20429cfc0a80e0948631974fa89c5dceade91a19
Esraa Alshaikh <esraaalshaikh200@gmail.com>
2026-03-12 16:46:37 +0300

    chore: dockerize backend and frontend with docker-compose

--- frontend/vehicle-frontend/src/app/environments/environment.ts
+++ frontend/vehicle-frontend/src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,  
-  apiBaseUrl: 'https://localhost:7100/api/vehicles'
+  production: false,
+  // apiBaseUrl: 'https://localhost:7100/api/vehicles'
+  apiBaseUrl: 'http://host.docker.internal:7100/api/vehicles',
 };
